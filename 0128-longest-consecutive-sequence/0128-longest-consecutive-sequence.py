dd89243ae11466daa15ae1dc89c99c659890836f
cryonicArtist <93345175+norisrivatsa@users.noreply.github.com>
2026-08-11 20:52:31 +0530

    26.9 MB (45.03%) 49 ms (76.8%) 08-11-2026 0128-longest-consecutive-sequence Medium .py Array

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            # Only start counting if num is the beginning
+            # of a consecutive sequence
+            if num - 1 not in num_set:
+                current = num
+                length = 1
+
+                while current + 1 in num_set:
+                    current += 1
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+                
+            
